4961f94b727c6ba2677ab15667db6cd86a4d5040
Dongyoung Lim <limdongyoung0@gmail.com>
2022-03-23 10:00:11 +0900

    Add tooltip width calculation function

--- static/chart-app/src/lib/util.ts
+++ static/chart-app/src/lib/util.ts
@@ -1,3 +1,20 @@
+import { ScatterDatum } from "../components/VictoryScatterPlot";
+
 export function getRandomFloat(min: number, max: number) {
   return Math.random() * (max - min) + min; //최댓값은 제외, 최솟값은 포함
+}
+
+export function calculateWidth(
+  datum: ScatterDatum,
+  xAxis: string,
+  yAxis: string,
+  zAxis: string
+): number {
+  const max_length = Math.max(
+    xAxis.length + String(datum.x).length,
+    yAxis.length + String(datum.y).length,
+    zAxis.length + String(datum.z).length,
+    datum.label!.length + 4
+  );
+  return max_length * 12;
 }
