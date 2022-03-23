b4bdcd98784655e7052e34929802323f6241ca96
Dongyoung Lim <limdongyoung0@gmail.com>
2022-03-23 10:17:35 +0900

    Implement ScatterPlotContainer to App

--- static/chart-app/src/App.tsx
+++ static/chart-app/src/App.tsx
@@ -1,7 +1,7 @@
 import { invoke, view } from "@forge/bridge";
 import React, { useEffect, useState } from "react";
 import ExcludedList from "./components/ExcludedList";
-import ScatterPlot from "./components/ScatterPlot";
+import ScatterPlotContainer from "./containers/ScatterPlotContainer";
 
 function App() {
   const [plotData, setPlotData]: any = useState([]);
@@ -38,11 +38,17 @@ function App() {
           <p>Error occured. Please check your jql or fields.</p>
         ) : (
           <>
-            <ScatterPlot
+            {/* <ScatterPlot
               plotData={plotData}
               xAxisName={JSON.parse(config.xAxis).name}
               yAxisName={JSON.parse(config.yAxis).name}
               zAxisName={JSON.parse(config.zAxis).name}
+            /> */}
+            <ScatterPlotContainer
+              plotData={plotData}
+              xAxis={JSON.parse(config.xAxis).name}
+              yAxis={JSON.parse(config.yAxis).name}
+              zAxis={JSON.parse(config.zAxis).name}
             />
             {excludedData.length > 0 && <ExcludedList data={excludedData} />}
           </>
