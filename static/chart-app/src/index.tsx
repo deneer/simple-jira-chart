c657508410fee5e6441a179725eabfc583b88bb5
Dongyoung Lim <limdongyoung0@gmail.com>
2022-03-23 12:07:56 +0900

    Import nanum gothic

--- static/chart-app/src/index.tsx
+++ static/chart-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import "@fontsource/nanum-gothic";
 // import "@atlaskit/css-reset";
 
 ReactDOM.render(
