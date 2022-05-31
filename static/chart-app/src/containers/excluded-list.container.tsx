5c8fadb684cebff2b66ce7c6eca2d2d8350d92e5
Dongyoung Lim <limdongyoung0@gmail.com>
2022-05-31 14:22:13 +0900

    Add excluded list container

---
+++ static/chart-app/src/containers/excluded-list.container.tsx
@@ -0,0 +1,16 @@
+import ListItem from "../components/list-item";
+
+function ExcludedListContainer() {
+  return (
+    <ul>
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+    </ul>
+  );
+}
+
+export default ExcludedListContainer;
