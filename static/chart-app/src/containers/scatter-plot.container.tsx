7e10c09804d85e40bb5c8eb283283ae1589368cd
Dongyoung Lim <limdongyoung0@gmail.com>
2022-05-31 14:22:18 +0900

    Add scatter plot container

---
+++ static/chart-app/src/containers/scatter-plot.container.tsx
@@ -0,0 +1,11 @@
+import ScatterPlot from "../components/scatter-plot";
+
+function ScatterPlotContainer() {
+  return (
+    <div>
+      <ScatterPlot />
+    </div>
+  );
+}
+
+export default ScatterPlotContainer;
