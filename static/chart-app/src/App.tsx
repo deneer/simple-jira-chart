5302650f01cba58f34c1616aa71b9e6ee220e80a
Dongyoung Lim <limdongyoung0@gmail.com>
2022-05-31 16:15:00 +0900

    Remove useEffect and provider

--- static/chart-app/src/App.tsx
+++ static/chart-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Provider, useAtom } from "jotai";
+import { atom, Provider, useAtom, useAtomValue } from "jotai";
 import { Suspense, useEffect } from "react";
 import ExcludedListContainer from "./containers/excluded-list.container";
 import ScatterPlotContainer from "./containers/scatter-plot.container";
@@ -6,27 +6,18 @@ import { jiraConfigAtom, jiraIssuesAtom } from "./store/atoms/jira.atom";
 import { getIssues, getJiraContext } from "./util/jira-util";
 
 function App() {
-  const [config, setConfig] = useAtom(jiraConfigAtom);
-  const [issues, setIssues] = useAtom(jiraIssuesAtom);
+  const config = useAtomValue(jiraConfigAtom);
+  const issues = useAtomValue(jiraIssuesAtom);
 
-  useEffect(() => {
-    setConfig(getJiraContext());
-    setIssues(getIssues());
-  }, []);
-
-  console.log(config);
   console.log(issues);
+  console.log(config);
 
   return (
-    <Provider>
-      <Suspense fallback="Loading...">
-        <div className="flex flex-col w-max items-center mr-auto ml-auto">
-          <h1 className="font-bold text-2xl text-blue-900">New Chart!</h1>
-          <ScatterPlotContainer />
-          <ExcludedListContainer />
-        </div>
-      </Suspense>
-    </Provider>
+    <div className="flex flex-col w-max items-center mr-auto ml-auto">
+      <h1 className="font-bold text-2xl text-blue-900">New Chart!</h1>
+      <ScatterPlotContainer />
+      <ExcludedListContainer />
+    </div>
   );
 }
 
