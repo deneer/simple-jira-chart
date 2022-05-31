b6a49733b07fc64286bfcde9766ce703400b0585
Dongyoung Lim <limdongyoung0@gmail.com>
2022-05-31 16:14:47 +0900

    Make atoms to initialize with async fetching

--- static/chart-app/src/store/atoms/jira.atom.ts
+++ static/chart-app/src/store/atoms/jira.atom.ts
@@ -1,5 +1,6 @@
 import { atom } from "jotai";
+import { getIssues, getJiraContext } from "../../util/jira-util";
 
-export const jiraConfigAtom = atom<any>({});
+export const jiraConfigAtom = atom<any>(async (get) => getJiraContext());
 
-export const jiraIssuesAtom = atom<any>([{}]);
+export const jiraIssuesAtom = atom<any>(async (get) => getIssues());
