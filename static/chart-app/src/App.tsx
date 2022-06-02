04d39c0d6d9c1afea21aef3646f6bc7e7098724e
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-02 16:59:05 +0900

    Remove useless console log and import

--- static/chart-app/src/App.tsx
+++ static/chart-app/src/App.tsx
@@ -1,12 +1,7 @@
-import { Suspense, useEffect } from "react";
 import ExcludedListContainer from "./containers/excluded-list.container";
 import ScatterPlotContainer from "./containers/scatter-plot.container";
-import { getIssues, getJiraContext } from "./util/jira-util";
 
 function App() {
-  console.log(issues);
-  console.log(config);
-
   return (
     <div className="flex flex-col w-max items-center mr-auto ml-auto">
       <h1 className="font-bold text-2xl text-blue-900">New Chart!</h1>
