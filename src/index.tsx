4a772d430341ee6a9607884689fb13a07f4e779b
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-02 11:57:54 +0900

    Applied new type to bridge resolver

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import Resolver from "@forge/resolver";
 import ForgeUI, { render } from "@forge/ui";
 import ChartConfig from "./components/ChartConfig";
 import { getJiraIssuesWithJql, getServerInfo } from "./lib/api";
+import { ChartPluginResponse } from "./types/chart-plugin-response.type";
 
 const resolver = new Resolver();
 
@@ -21,16 +22,26 @@ resolver.define("getIssues", async (req) => {
   }
   const jiraResponse = await getJiraIssuesWithJql(config.jql);
 
+  // TODO: organize code here. so dirty.
   return config.xAxis && config.yAxis && config.zAxis
     ? jiraResponse
       ? {
-          payload: jiraResponse.map((issue) => {
+          payload: jiraResponse.map((issue): ChartPluginResponse => {
             return {
               x: issue.fields[JSON.parse(config.xAxis).key],
               y: issue.fields[JSON.parse(config.yAxis).key],
-              z: issue.fields[JSON.parse(config.zAxis).key],
-              title: issue.fields.summary,
+              size: issue.fields[JSON.parse(config.zAxis).key],
+              summary: issue.fields.summary,
               issueKey: issue.key,
+              status: {
+                name: issue.fields.status.name,
+                statusCategory: {
+                  key: issue.fields.status.statusCategory.key,
+                  colorName: issue.fields.status.statusCategory.colorName,
+                  name: issue.fields.status.statusCategory.name,
+                },
+              },
+              assignee: issue.fields.assignee,
             };
           }),
           base: baseUrl,
