a40f818024f0fb9a53580ddf8fd208a3c22ac7db
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-03 15:58:08 +0900

    Add jira status hex color enum

--- static/chart-app/src/types/jira-status-color.enum.ts
+++ static/chart-app/src/types/jira-status-color.enum.ts
@@ -4,3 +4,10 @@ export enum JiraStatusTailwindColor {
   DONE_COLOR = "bg-green-200",
   DEFAULT_COLOR = "bg-white",
 }
+
+export enum JiraStatusHexColor {
+  TODO_COLOR = "#23a3e8",
+  IN_PROGRESS_COLOR = "#e8de23",
+  DONE_COLOR = "#59d484",
+  DEFAULT_COLOR = "#b023e8",
+}
