698ba9327c398f37b9acb89a845de15ab438bf77
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-03 10:23:29 +0900

    Apply excluded list to container

--- static/chart-app/src/containers/excluded-list.container.tsx
+++ static/chart-app/src/containers/excluded-list.container.tsx
@@ -1,15 +1,23 @@
+import { useAtomValue } from "jotai";
+import ExcludedList from "../components/excluded-list";
 import ListItem from "../components/list-item";
+import {
+  excludedJiraIssuesAtom,
+  jiraConfigAtom,
+} from "../store/atoms/jira.atom";
 
 function ExcludedListContainer() {
+  const excludedIssuesData = useAtomValue(excludedJiraIssuesAtom);
+  const jiraConfig = useAtomValue(jiraConfigAtom);
+
   return (
-    <ul>
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-    </ul>
+    <ExcludedList
+      data={excludedIssuesData}
+      baseUrl={jiraConfig.siteUrl}
+      xAxis={jiraConfig.extension.config.xAxis.name}
+      yAxis={jiraConfig.extension.config.yAxis.name}
+      sizeAxis={jiraConfig.extension.config.zAxis.name}
+    />
   );
 }
 
