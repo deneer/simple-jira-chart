f7ad20cf26399886dccfea8401fd3e0295db6350
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-08 11:07:31 +0900

    Set type to filteredJiraIssuesAtom

--- static/chart-app/src/store/atoms/jira.atom.ts
+++ static/chart-app/src/store/atoms/jira.atom.ts
@@ -1,5 +1,6 @@
 import { atom } from "jotai";
 import { BridgeResponse } from "../../types/bridge-response.type";
+import { ChartPluginResponse } from "../../types/chart-plugin-response.type";
 import { getIssues, getJiraContext } from "../../util/jira-util";
 
 export const jiraConfigAtom = atom<any>(async (get) => getJiraContext());
@@ -8,8 +9,8 @@ export const jiraIssuesAtom = atom<Promise<BridgeResponse>>(async (get) =>
   getIssues()
 );
 
-export const filteredJiraIssuesAtom = atom((get) =>
-  get(jiraIssuesAtom).payload.filter((issue: any) => issue.x && issue.y)
+export const filteredJiraIssuesAtom = atom<ChartPluginResponse[]>((get) =>
+  get(jiraIssuesAtom).payload.filter((issue) => issue.x && issue.y)
 );
 
 export const excludedJiraIssuesAtom = atom((get) =>
