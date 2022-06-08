40349696b40486b63b7182c18a854285b202aaef
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-08 17:28:33 +0900

    Remove new chart text

--- static/chart-app/src/App.tsx
+++ static/chart-app/src/App.tsx
@@ -4,7 +4,6 @@ import ScatterPlotContainer from "./containers/scatter-plot.container";
 function App() {
   return (
     <div className="flex flex-col w-full items-center mr-auto ml-auto">
-      <h1 className="font-bold text-2xl text-blue-900">New Chart!</h1>
       <ScatterPlotContainer />
       <ExcludedListContainer />
     </div>
