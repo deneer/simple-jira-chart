4b5e2eaf8b3ac4c215bb6a39a528477d0fec6407
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-08 11:06:41 +0900

    Set BridgeResponse type to jiraIssuesAtom

--- static/chart-app/src/store/atoms/jira.atom.ts
+++ static/chart-app/src/store/atoms/jira.atom.ts
@@ -1,9 +1,12 @@
 import { atom } from "jotai";
+import { BridgeResponse } from "../../types/bridge-response.type";
 import { getIssues, getJiraContext } from "../../util/jira-util";
 
 export const jiraConfigAtom = atom<any>(async (get) => getJiraContext());
 
-export const jiraIssuesAtom = atom<any>(async (get) => getIssues());
+export const jiraIssuesAtom = atom<Promise<BridgeResponse>>(async (get) =>
+  getIssues()
+);
 
 export const filteredJiraIssuesAtom = atom((get) =>
   get(jiraIssuesAtom).payload.filter((issue: any) => issue.x && issue.y)
