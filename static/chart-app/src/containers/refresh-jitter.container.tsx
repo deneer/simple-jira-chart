a44aaa16cf365a262f81f59caa56446eb7c8bd52
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-14 16:42:21 +0900

    Apply refresh issue to refresh jitter container

--- static/chart-app/src/containers/refresh-jitter.container.tsx
+++ static/chart-app/src/containers/refresh-jitter.container.tsx
@@ -1,15 +1,24 @@
 import { useAtom } from "jotai";
-import { resetJitterAtom } from "../store/atoms/jira.atom";
+import { resetIssuesAtom, resetJitterAtom } from "../store/atoms/jira.atom";
 
 function RefreshJitterContainer() {
   const [reset, resetJitter] = useAtom(resetJitterAtom);
+  const [refresh, refreshIssues] = useAtom(resetIssuesAtom);
   return (
-    <button
-      className="w-full bg-blue-100 hover:bg-blue-200 text-blue-900 font-bold py-2 px-4 rounded inline-flex items-center justify-center"
-      onClick={() => resetJitter(!reset)}
-    >
-      Refresh Scatter Jittering(점 다시 뿌리기)
-    </button>
+    <div className="w-full flex justify-between mb-2">
+      <button
+        className="bg-blue-100 hover:bg-blue-200 text-blue-900 font-bold py-2 px-4 rounded inline-flex items-center justify-center text-xs"
+        onClick={() => refreshIssues(!refresh)}
+      >
+        Refresh Issues
+      </button>
+      <button
+        className="bg-blue-100 hover:bg-blue-200 text-blue-900 font-bold py-2 px-4 rounded inline-flex items-center justify-center text-xs"
+        onClick={() => resetJitter(!reset)}
+      >
+        Refresh Scatter Jittering(점 다시 뿌리기)
+      </button>
+    </div>
   );
 }
 
