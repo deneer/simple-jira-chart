0904e0fcbc46088ac229d7bf3efd22c6e94b742b
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-14 13:08:00 +0900

    Add opacity range slider container

---
+++ static/chart-app/src/containers/opacity-range-slider.container.tsx
@@ -0,0 +1,23 @@
+import { useAtom } from "jotai";
+import { RangeSlider } from "../components/range-slider";
+import { opacityAtom } from "../store/atoms/scatter-plot.atom";
+
+export function OpacityRangeSliderContainer() {
+  const [opacity, setOpacity] = useAtom(opacityAtom);
+
+  const handleOpacityChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setOpacity(+e.target.value);
+
+  return (
+    <div className="w-full flex justify-between items-center mt-2">
+      <p className="mr-4 w-24">투명도 조절</p>
+      <RangeSlider
+        value={opacity}
+        min={0}
+        max={1}
+        step={0.01}
+        handleChange={handleOpacityChange}
+      />
+    </div>
+  );
+}
