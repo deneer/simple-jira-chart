862da9a6fb2d8f1dfd98471df0e12411b0e204b1
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-16 18:33:45 +0900

    Fix min width of plugin to 600px

--- static/chart-app/src/App.tsx
+++ static/chart-app/src/App.tsx
@@ -5,7 +5,7 @@ import ScatterPlotContainer from "./containers/scatter-plot.container";
 
 function App() {
   return (
-    <div className="flex flex-col w-3/5 min-w-xl items-center mr-auto ml-auto">
+    <div className="flex flex-col w-3/5 min-w-[600px] items-center mr-auto ml-auto">
       <RefreshContainer />
       <ScatterPlotContainer />
       <OpacityRangeSliderContainer />
