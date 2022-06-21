a10a1f991b7c498c4cebf54eca77a1b1cc6e82f9
D0Lim <limdongyoung0@gmail.com>
2022-06-21 11:10:03 +0900

    Feature resize scatter (#34)
    * Add atom for adjusting size unit
    
    * Add size unit to scatter plot component
    
    * Add size unit logic to opacity range slider container
    
    * Pass size unit from container

--- static/chart-app/src/containers/opacity-range-slider.container.tsx
+++ static/chart-app/src/containers/opacity-range-slider.container.tsx
@@ -1,23 +1,39 @@
 import { useAtom } from "jotai";
 import { RangeSlider } from "../components/range-slider";
-import { opacityAtom } from "../store/atoms/scatter-plot.atom";
+import { opacityAtom, sizeUnitAtom } from "../store/atoms/scatter-plot.atom";
 
 export function OpacityRangeSliderContainer() {
   const [opacity, setOpacity] = useAtom(opacityAtom);
+  const [sizeUnit, setSizeUnit] = useAtom(sizeUnitAtom);
 
   const handleOpacityChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setOpacity(+e.target.value);
 
+  const haneldSizeUnitChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSizeUnit(+e.target.value);
+
   return (
-    <div className="w-full flex justify-between items-center mt-2 bg-blue-100 font-bold text-xs text-blue-900 rounded p-2">
-      <p className="mr-4 w-24">투명도 조절</p>
-      <RangeSlider
-        value={opacity}
-        min={0}
-        max={1}
-        step={0.01}
-        handleChange={handleOpacityChange}
-      />
+    <div className="flex">
+      <div className="w-full flex justify-between items-center mt-2 bg-blue-100 font-bold text-xs text-blue-900 rounded p-2 mr-2">
+        <p className="mr-4 w-24">투명도 조절</p>
+        <RangeSlider
+          value={opacity}
+          min={0}
+          max={1}
+          step={0.01}
+          handleChange={handleOpacityChange}
+        />
+      </div>
+      <div className="w-full flex justify-between items-center mt-2 bg-blue-100 font-bold text-xs text-blue-900 rounded p-2">
+        <p className="mr-4 w-24">점 크기 조절</p>
+        <RangeSlider
+          value={sizeUnit}
+          min={10}
+          max={1000}
+          step={1}
+          handleChange={haneldSizeUnitChange}
+        />
+      </div>
     </div>
   );
 }
