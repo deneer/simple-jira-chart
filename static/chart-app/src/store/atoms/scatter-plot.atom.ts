49b10f7d1f16193cc0ea6a631b8cb89fa2ae0746
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-21 13:47:17 +0900

    Add order atoms

--- static/chart-app/src/store/atoms/scatter-plot.atom.ts
+++ static/chart-app/src/store/atoms/scatter-plot.atom.ts
@@ -3,3 +3,7 @@ import { atom } from "jotai";
 export const opacityAtom = atom<number>(0.6);
 
 export const sizeUnitAtom = atom<number>(200);
+
+export const xDomainAscendingAtom = atom<boolean>(true);
+
+export const yDomainAscendingAtom = atom<boolean>(true);
