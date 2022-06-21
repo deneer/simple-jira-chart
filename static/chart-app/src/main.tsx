7d4701e65b94a43db35c072834634df1daedcdc3
D0Lim <limdongyoung0@gmail.com>
2022-06-21 11:35:13 +0900

    Feature skeleton (#35)
    * Implement loading skeleton
    
    * Apply loading skeleton as suspense component

--- static/chart-app/src/main.tsx
+++ static/chart-app/src/main.tsx
@@ -2,12 +2,13 @@ import { Provider } from "jotai";
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import LoadingSkeleton from "./components/loading-skeleton";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
-      <Suspense fallback={<h2>Loading Data...</h2>}>
+      <Suspense fallback={<LoadingSkeleton />}>
         <App />
       </Suspense>
     </Provider>
