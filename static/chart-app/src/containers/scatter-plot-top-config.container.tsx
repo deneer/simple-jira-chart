0563e87b9bd50b5e7b878937c32c632108680355
Dongyoung Lim <limdongyoung0@gmail.com>
2022-06-24 14:32:55 +0900

    Add scale button group

--- static/chart-app/src/containers/scatter-plot-top-config.container.tsx
+++ static/chart-app/src/containers/scatter-plot-top-config.container.tsx
@@ -19,12 +19,27 @@ function ScatterPlotTopConfigContainer({
       >
         Refresh Issues
       </button>
-      <button
-        className="bg-blue-100 hover:bg-blue-200 text-blue-900 font-bold py-2 px-4 rounded inline-flex items-center justify-center text-xs"
-        onClick={() => zoom.reset()}
-      >
-        Reset Zoom
-      </button>
+      <div className="flex rounded overflow-hidden">
+        <button
+          className="bg-blue-100 hover:bg-blue-200 text-blue-900 font-bold py-2 px-4 inline-flex items-center justify-center text-sm"
+          onClick={() => zoom.scale({ scaleX: 0.8, scaleY: 0.8 })}
+        >
+          -
+        </button>
+        <button
+          className="bg-blue-100 hover:bg-blue-200 text-blue-900 font-bold py-2 px-4 inline-flex items-center justify-center text-xs"
+          onClick={() => zoom.reset()}
+        >
+          Reset Zoom
+        </button>
+        <button
+          className="bg-blue-100 hover:bg-blue-200 text-blue-900 font-bold py-2 px-4 inline-flex items-center justify-center text-sm"
+          onClick={() => zoom.scale({ scaleX: 1.2, scaleY: 1.2 })}
+        >
+          +
+        </button>
+      </div>
+
       <button
         className="bg-blue-100 hover:bg-blue-200 text-blue-900 font-bold py-2 px-4 rounded inline-flex items-center justify-center text-xs"
         onClick={() => resetJitter(!reset)}
